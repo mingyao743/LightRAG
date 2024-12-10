098806d5a455a74d557a73e5ee58fc1fa62ee47e
MingyaoChen <84067629+mingyao743@users.noreply.github.com>
2024-12-10 11:07:23 +0800

    Update file_utils.py

--- lightrag/file_utils.py
+++ lightrag/file_utils.py
@@ -1,5 +1,7 @@
-import re
+import re, os
 import jionlp as jio
+from moviepy.editor import VideoFileClip
+from bs4 import BeautifulSoup
 
 def merge_newlines(text):
     return re.sub(r'\n+', '\n', text)
@@ -22,5 +24,22 @@ def percent_of_chinese(text):
             chinese_count += 1.
     return chinese_count / char_count
 
+def download_video(url, out_dir):
+    os.system(f'you-get -o {out_dir} {url}') # use you-get download video
+    files = [os.path.join(our_dir, f) for f in os.listdir(outdir) if f.endswith('mp4')]
+    return files
 
+def extract_audio_from_video(video_path, out_path):
+    clip = VideoFileClip(video_path)
+    clip.audio.write_audiofile(out_path)
 
+def extract_content_from_xhs(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content)
+    items = soup.find_all('div', {'class': 'note-content'})
+    text = items[0].text if len(items) > 0 else ''
+    return text
+
+class EpubExtracter():
+    def __init__(self):
+        """ """
