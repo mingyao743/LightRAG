0eb849d2c0e41002ac4a30b2895d65cffdc5159b
MingyaoChen <84067629+mingyao743@users.noreply.github.com>
2024-12-10 10:58:42 +0800

    Update file_utils.py

--- lightrag/file_utils.py
+++ lightrag/file_utils.py
@@ -1,1 +1,26 @@
+import re
+import jionlp as jio
+
+def merge_newlines(text):
+    return re.sub(r'\n+', '\n', text)
+
+def clean_content(content):
+    content = merge_newlines(content)
+    content = content.strip()
+    content = jio.remove_exception_char(content)
+    content = jio.remove_redundant_char(content)
+    return content
+
+def percent_of_chinese(text):
+    char_count = len(text)
+    if char_count == 0:
+        return 0.
+      
+    chinese_count = 0.
+    for c in text:
+        if jio.check_any_chinese_char(c):
+            chinese_count += 1.
+    return chinese_count / char_count
+
+
 
